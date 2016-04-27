23869a932e875233b238459446ddb74645bc749e
xyk0058 <13301007@bjtu.edu.cn>
2016-04-27 20:18:01 +0800

    xyk

---
+++ src/com/dhcc/Global/Functions.java
@@ -0,0 +1,27 @@
+package com.dhcc.Global;
+
+public class Functions {
+	
+	//n is gen number 
+	private static double f(double x, int n) {
+		double ret = 0.0;
+		switch (n) {
+			case 1 :
+				ret = x;
+				break;
+			case 2 :
+				break;
+			default :
+				ret = 0;
+		}
+		return ret;
+	}
+	
+	public static double F(double[] X) {
+		double ret = 0;
+		for (int i=0; i<X.length; i++) {
+			ret += f(X[i], i);
+		}
+		return ret;
+	}
+}
